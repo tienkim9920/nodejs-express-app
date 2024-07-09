31b8cb707bbdf782fa678a51f9f05f7a112b31e8
Tien Kim <tienkim9920@gmail.com>
2024-07-09 23:51:19 +0700

    update

---
+++ index.js
@@ -0,0 +1,50 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+const bodyParser = require("body-parser");
+
+const hello = [
+  {
+    id: 1,
+    name: "Ninedev",
+  },
+  {
+    id: 2,
+    name: "2024",
+  },
+];
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.json("Ninedev 2024");
+});
+
+app.get("/detail/:id", (req, res) => {
+  const { id } = req.params;
+  res.json(`Ninedev 2024 id: ${hello.findIndex((item) => item.id == id)}`);
+});
+
+app.get("/courses/:id", (req, res) => {
+  const { name, page } = req.query;
+  const { id } = req.params;
+  res.json(`name: ${name}, page: ${page}, courseId: ${id}`);
+});
+
+app.post("/create", (req, res) => {
+  const body = req.body;
+  res.json(body);
+});
+
+app.put("/update", (req, res) => {
+  res.json("Update");
+});
+
+app.delete("/delete", (req, res) => {
+  res.json("Delete");
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}/`);
+});
